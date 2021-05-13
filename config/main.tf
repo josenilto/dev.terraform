6fffac1e929de62d132fee3bae370ae2366104c0
josenilto <josenilto@outlook.com>
2021-05-12 21:03:59 -0300

    master

--- config/main.tf
+++ config/main.tf
@@ -7,7 +7,7 @@ resource "aws_intance" "dev"{
     count = 1
     ami = "ami-00399ec92321828f5"
     instance_type= "t2.micro"
-    key
+    key_name = "josenilto.luis"
     tags = {
         Name = "DEV${count.index}"
     }
